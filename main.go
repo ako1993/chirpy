3bf722006777c8e647f87a7a39031846cea99977
andreas <andre@LAPTOP-61UG31CH.localdomain>
2026-07-10 18:32:53 -0400

    Added json validation function and handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -42,11 +43,70 @@ func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverhits.Store(0)
 }
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	// Create helper function to return error for bad JSON
+	type returnErr struct {
+		ErrMessage string `json:"error"`
+	}
+	errBody := returnErr{
+		ErrMessage: message,
+	}
+	dat, err := json.Marshal(errBody)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	//Create helper function to respond to a good request with JSON
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func validate_chirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp too long!")
+	} else {
+		type returnJson struct {
+			Is_valid bool `json:"valid"`
+		}
+		jsonBody := returnJson{
+			Is_valid: true,
+		}
+		respondWithJSON(w, 200, jsonBody)
+	}
+
+}
+
 func main() {
 	const filepathroot = "."
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathroot)))))
 	mux.HandleFunc("GET /api/healthz", ServeHttp)
+	mux.HandleFunc("POST /api/validate_chirp", validate_chirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.WriteHits)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHits)
 	server := &http.Server{
